fb66b6352db30b9ff4f27f905992013a1f1d3db9
netghost69 <martin_saltiel@yahoo.co.uk>
2020-04-09 03:30:37 +0100

    Added inital sketch

---
+++ car-tyre-temperature.ino
@@ -0,0 +1,41 @@
+
+
+
+
+// Sketch created by M.Saltiel 24 March 2020 00:41
+// Project using an Arduino, 4xGY06 IR Temperature
+// sensors and an 0.96" OLED display.
+//
+// The tyres are Dunlop DZ03G Medium compound the
+// temperature should show when the tyre is out of
+// it's temperature range.(Flash or invert) I used
+// a blue OLED as the car has blue interior
+// lighting.
+//////////////////////////////////////////////////
+
+// Includes are std ones for each item.
+
+#include <SPI.h>
+#include <Wire.h>
+
+// I may use a different OLED library but I know this works
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+#include <Adafruit_SSD1306.h>
+
+// The below were added by the IDE but I don't use them
+//#include <Adafruit_SPITFT_Macros.h>
+//#include <Adafruit_SPITFT.h>
+//#include <splash.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
